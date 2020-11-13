10ce445ce258a829ec83f7b15836ca1eac97b7b3
robi <robi_jeanne_banogon@dlsu.edu.ph>
2020-11-13 18:46:35 +0800

    added react google

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
+
+const responseGoogle = (response) => {
+  console.log('hellooo ddd');
+  console.log(response);
+  console.log(response.profileObj);
+};
 
 const theme = createMuiTheme({
   palette: {
@@ -47,9 +54,24 @@ export default function Landing() {
             </h5>
           </Grid>
           <Grid item>
-            <Button variant="contained" color="primary" className={{ textTransform: 'uppercase' }}>
-              Sign In
-            </Button>
+            <GoogleLogin
+              clientId="498581729039-9633ego9k719cs5a2rq44s6dgcrmi2rb.apps.googleusercontent.com"
+              render={(renderProps) => (
+                <Button
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  variant="contained"
+                  color="primary"
+                  className={{ textTransform: 'uppercase' }}
+                >
+                  Sign In With Google
+                </Button>
+              )}
+              buttonText="Sign-in"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
           </Grid>
         </Grid>
       </Container>
