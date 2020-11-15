3540d504519c6024f4fac97d8e0e1d26fbfde440
kenloquinte <kennethloquinte@gmail.com>
2020-11-15 16:49:02 +0800

    add frontend validation for register form

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
 export default function RegistrationForm() {
+  const {
+    register,
+    errors,
+    handleSubmit,
+    formState,
+  } = useForm({ criteriaMode: 'all', mode: 'onChange' });
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Grid
         container
         direction="column"
@@ -14,28 +24,32 @@ export default function RegistrationForm() {
         <Grid item>
           <TextField
             label="Contact Number"
+            name="contact"
             variant="filled"
             fullWidth
             required
-            inputProps={{ maxlength: '15' }}
+            inputProps={{ maxLength: '15' }}
+            inputRef={register({ required: 'This field is required.', pattern: { value: /^[0-9+]+$/, message: 'Only 0-9 and + symbols allowed.' } })}
+            helperText={<ErrorMessage errors={errors} name="contact" />}
           />
         </Grid>
-
         <Grid item>
           <TextField
             multiline
+            name="bio"
             label="Bio"
             variant="filled"
             fullWidth
             rows={3}
             rowsMax={6}
-            inputProps={{ maxlength: '140' }}
+            inputProps={{ maxLength: '140' }}
+            inputRef={register}
 
           />
         </Grid>
 
         <Grid item>
-          <Button color="primary" variant="contained"> Confirm </Button>
+          <Button color="primary" variant="contained" disabled={!formState.isValid} type="submit"> Confirm </Button>
         </Grid>
       </Grid>
     </form>
