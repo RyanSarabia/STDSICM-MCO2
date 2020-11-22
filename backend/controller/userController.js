af05150d62639b5cd81c087deb371028a40bfe84
robi <robi_jeanne_banogon@dlsu.edu.ph>
2020-11-22 20:50:26 +0800

    added auction model

---
+++ backend/controller/userController.js
@@ -0,0 +1,6 @@
+// const User = require('../model/user.model');
+// const Auction = require('../model/auction.model');
+
+// exports.getAuction = async function getAuction(req, res) {};
+
+// exports.postCreateAuction = async function postAuction(req, res) {};
