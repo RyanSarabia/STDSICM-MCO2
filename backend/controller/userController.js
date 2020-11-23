47f3d191a1afa87b7f6dd8b94a5c3db9f93bc725
robi <robi_jeanne_banogon@dlsu.edu.ph>
2020-11-23 17:51:36 +0800

    added cloudinary module

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,4 +3,23 @@
 
 // exports.getAuction = async function getAuction(req, res) {};
 
-// exports.postCreateAuction = async function postAuction(req, res) {};
+// exports.postCreateAuction = async function postAuction(req, res) {
+
+//     const user = await User.findOne({ email: req.session.passport.user.profile.emails[0].value });
+//     if (user){
+//         const newAuction = new Auction({
+//             title: req.body.title,
+//             description: req.body.description,
+//             cutofftime: req.body.,
+//             cutoffdate: req.body.,
+//             startprice: req.body.startPrice,
+//             incprice: req.body.increment,
+//             stealprice: req.body.stealPrice,
+//             photos: req.body.photos,
+//         });
+
+//         auction = await newAuction.save();
+//         user.auctions = auction;
+
+//     }
+// };
