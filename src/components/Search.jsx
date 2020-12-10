3f531abc7f9e5b043872c2d1d9b9a81295825ab0
kenloquinte <kennethloquinte@gmail.com>
2020-12-10 17:45:21 +0800

    add search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,50 @@
+/* eslint-disable react/jsx-no-duplicate-props */
+import React, { useState } from 'react';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FormControl from '@material-ui/core/FormControl';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default function Search() {
+  const [searchInput, setSearch] = useState('');
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (searchInput !== '') {
+      console.log(searchInput);
+      // code here for results page
+    }
+  };
+
+  const handleEnterPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <FormControl variant="outlined">
+      <OutlinedInput
+        name="search"
+        placeholder="Search"
+        // variant="outlined"
+        inputProps={{ maxLength: '30' }}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton aria-label="search" onClick={handleSubmit}>
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        }
+        style={{ width: '30vw' }}
+        value={searchInput}
+        onChange={handleChange}
+        onKeyPress={handleEnterPress}
+      />
+    </FormControl>
+  );
+}
