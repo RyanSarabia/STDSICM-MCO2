92f3bd7b73aed0ec52c27272142205d12135402a
kenloquinte <kennethloquinte@gmail.com>
2020-12-15 00:02:41 +0800

    removed unecessary comments

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -26,32 +26,8 @@ export default function Explore() {
   const [auctions, setAuctions] = useState('');
   const [auctionCount, setCount] = useState('');
   const location = useLocation();
-  // const query = new URLSearchParams(location.search);
 
   useEffect(() => {
-    // console.log(query.get('search'));
-
-    // <Route>
-    //   {({ location }) => {
-    //     const query = new URLSearchParams(location.search);
-    //     const page = parseInt(query.get('page') || '1', 10);
-    //     return (
-    //       <Pagination
-    //         page={page}
-    //         count={10}
-    //         renderItem={(item) => (
-    //           <PaginationItem
-    //             component={Link}
-    //             to={`/explore${item.page === 1 ? '' : `?page=${item.page}`}`}
-    //             // eslint-disable-next-line react/jsx-props-no-spreading
-    //             {...item}
-    //           />
-    //         )}
-    //       />
-    //     );
-    //   }}
-    // </Route>;
-
     axios.get(`/explore/getAllAuction${location.search}`).then((res) => {
       let i;
 
@@ -65,7 +41,6 @@ export default function Explore() {
 
       setAuctions(res.data.auctions);
       setCount(res.data.count);
-      console.log(res.data);
       window.scrollTo(0, 0);
     });
   }, [location]);
@@ -105,7 +80,6 @@ export default function Explore() {
           <PaginationBar pageCount={Math.ceil(auctionCount / 10, 10)} />
         </Grid>
       )}
-      {/* {console.log(Math.ceil(auctionCount / 10, 10))} */}
     </div>
   );
 }
