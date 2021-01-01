ac1c3614b53f56dbc894eddcc94e0cf7a0a24515
sharmainegaw <sharmaine_gaw@dlsu.edu.ph>
2021-01-01 14:50:08 +0800

    add test for format date function

---
+++ src/myFunctions.js
@@ -0,0 +1,17 @@
+const formatDate = (oldDate) => {
+    let newDate = (oldDate.getMonth() + 1) < 10 ? "0" + (oldDate.getMonth() + 1) : (oldDate.getMonth() + 1);
+    newDate += '/';
+    newDate += oldDate.getDate() < 10 ? "0" + oldDate.getDate() : oldDate.getDate();
+    newDate += '/';
+    newDate += oldDate.getFullYear();
+    newDate += ' | ';
+    newDate += oldDate.getHours() < 10 ? "0" + oldDate.getHours() : oldDate.getHours();
+    newDate += ':';
+    newDate += oldDate.getMinutes() < 10 ? "0" + oldDate.getMinutes() : oldDate.getMinutes();
+    newDate += ':';
+    newDate += oldDate.getSeconds() < 10 ? "0" + oldDate.getSeconds() : oldDate.getSeconds();
+
+    return newDate;
+};
+
+exports.formatDate = formatDate;
