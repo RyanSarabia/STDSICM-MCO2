be6faad3ac64f290abe07c7b2bb26f8ae521a1a6
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2021-01-05 20:24:01 +0800

    showing data on frontend

--- src/components/Auction.jsx
+++ src/components/Auction.jsx
@@ -1,24 +1,73 @@
-// import React, { useState, useEffect } from 'react';
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
 import { useParams } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
 
 export default function Auction() {
+  const [auction, setAuction] = useState('');
   const auctionId = useParams().auction;
 
   console.log(auctionId);
+  console.log(auction);
 
   useEffect(() => {
-    axios.get(`/explore/getAuction/${auctionId}`).then((res) => {
+    axios.get(`/auction/getAuction/${auctionId}`).then((res) => {
       const tempdata = res.data;
       console.log(tempdata);
+      setAuction(tempdata);
     });
   }, []);
 
   return (
-    <div>
-      <div> Buboi </div>
-    </div>
+    <Grid container="true" direction="row">
+      <Card
+        elevation={3}
+        style={{
+          maxWidth: '50%',
+          minWidth: '50%',
+          marginLeft: '3vw',
+          marginTop: '3vw',
+          marginBottom: '3vw',
+          padding: '1vw',
+        }}
+      >
+        <Grid container="true">
+          <Typography>{auction.title}</Typography>
+          <Typography>{auction.postdate}</Typography>
+          <Typography>{auction.description}</Typography>
+          <Typography>{auction.cutoff}</Typography>
+          <CardMedia
+            style={{ height: 200, width: 200 }}
+            component="image"
+            image={auction.imageurl}
+          />
+        </Grid>
+      </Card>
+      <Card
+        elevation={3}
+        style={{ maxWidth: '35%', minWidth: '35%', margin: '3vw', padding: '1vw' }}
+      >
+        <Grid container="true">
+          <Typography>
+            Starting Bid:
+            {auction.startPrice}
+          </Typography>
+          <Typography>
+            Current Bid:
+            {auction.currentPrice}
+          </Typography>
+          <Typography>
+            Highest Bidder:
+            {auction.highestBidder}
+          </Typography>
+          <Button>Bid</Button>
+          <Button>Steal</Button>
+        </Grid>
+      </Card>
+    </Grid>
   );
 }
