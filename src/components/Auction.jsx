5822679179fa96a867affde23141df0c86ba1fad
BrianJSo <brian_jezreel_so@dlsu.edu.ph>
2021-01-06 21:11:05 +0800

    finished formatting for left card

--- src/components/Auction.jsx
+++ src/components/Auction.jsx
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
+// import Badge from '@material-ui/core/Badge';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
+import Chip from '@material-ui/core/Chip';
+import clsx from 'clsx';
+import TextField from '@material-ui/core/TextField';
 import { useParams } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { formatDate } from '../myFunctions';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  shape: {
+    backgroundColor: 'green',
+    width: '17px',
+    height: '17px',
+  },
+  shapeCircle: {
+    borderRadius: '50%',
+  },
+}));
 
 export default function Auction() {
+  const classes = useStyles();
+  const circle = <div className={clsx(classes.shape, classes.shapeCircle)} />;
   const [auction, setAuction] = useState('');
   const auctionId = useParams().auction;
 
@@ -18,6 +42,10 @@ export default function Auction() {
     axios.get(`/auction/getAuction/${auctionId}`).then((res) => {
       const tempdata = res.data;
       console.log(tempdata);
+
+      tempdata.postdate = formatDate(tempdata.postdate);
+      tempdata.cutoff = formatDate(tempdata.cutoff);
+
       setAuction(tempdata);
     });
   }, []);
@@ -33,41 +61,128 @@ export default function Auction() {
           marginTop: '3vw',
           marginBottom: '3vw',
           padding: '1vw',
+          borderRadius: '1vw',
         }}
       >
         <Grid container="true">
-          <Typography>{auction.title}</Typography>
-          <Typography>{auction.postdate}</Typography>
-          <Typography>{auction.description}</Typography>
-          <Typography>{auction.cutoff}</Typography>
-          <CardMedia
-            style={{ height: 200, width: 200 }}
-            component="image"
-            image={auction.imageurl}
-          />
+          <Grid item container="true" justify="space-between">
+            <Typography variant="h4">{auction.title}</Typography>
+            <Grid
+              item
+              container="true"
+              justify="flex-end"
+              alignItems="center"
+              style={{ width: '10vw' }}
+            >
+              {circle}
+              <Typography style={{ margin: '5px' }}>Status</Typography>
+            </Grid>
+          </Grid>
+          <Grid item container="true">
+            <Chip label={auction.postdate} variant="outlined" />
+          </Grid>
+          <Grid item container="true" style={{ marginTop: '1vw', marginBottom: '1vw' }}>
+            <Typography variant="h7">{auction.description}</Typography>
+          </Grid>
+          <Grid item direction="column" container="true">
+            <Typography style={{ fontWeight: 'bold' }}>Cut-off</Typography>
+            <Chip label={auction.postdate} variant="outlined" style={{ maxWidth: '160px' }} />
+          </Grid>
+          <Grid item container="true" justify="center">
+            <CardMedia
+              style={{
+                width: '100%',
+                height: '25vw',
+                objectFit: 'scale-down',
+                marginTop: '1vw',
+                borderRadius: '10px',
+              }}
+              component="image"
+              image={auction.imageurl}
+            />
+          </Grid>
         </Grid>
       </Card>
       <Card
         elevation={3}
-        style={{ maxWidth: '35%', minWidth: '35%', margin: '3vw', padding: '1vw' }}
+        style={{
+          maxWidth: '35%',
+          minWidth: '35%',
+          maxHeight: '16  vw',
+          margin: '3vw',
+          padding: '1vw',
+          borderRadius: '1vw',
+        }}
       >
-        <Grid container="true">
-          <Typography>
-            Starting Bid:
-            {auction.startPrice}
-          </Typography>
-          <Typography>
-            Current Bid:
-            {auction.currentPrice}
-          </Typography>
-          <Typography>
-            Highest Bidder:
-            {auction.highestBidder}
-          </Typography>
-          <Button>Bid</Button>
-          <Button>Steal</Button>
+        <Grid
+          container="true"
+          direction="column"
+          justify="space-arpund"
+          style={{ padding: '2vw', height: '100%' }}
+        >
+          <Grid item container="true" justify="space-around">
+            <Typography>
+              Starting Bid:
+              {auction.startPrice}
+            </Typography>
+            <Typography>
+              Current Bid:
+              {auction.currentPrice}
+            </Typography>
+          </Grid>
+          <Grid item container="true" justify="space-around" style={{ marginTop: '1vw' }}>
+            <Typography>
+              Highest Bidder:
+              {auction.highestBidder}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            container="true"
+            direction="row"
+            justify="center"
+            alignItems="center"
+            style={{ marginTop: '1vw' }}
+          >
+            <TextField
+              type="number"
+              variant="outlined"
+              min={parseInt(auction.currentPrice + auction.increment, 10)}
+              max={parseInt(auction.stealPrice - auction.increment, 10)}
+              step={parseInt(auction.increment, 10)}
+              defaultValue={parseInt(auction.currentPrice + auction.increment, 10)}
+              key={`${Math.floor(Math.random() * 1000)}-min`}
+              onKeyDown={(event) => {
+                event.preventDefault();
+              }}
+              style={{ width: '5vw', height: '100%', borderRadius: '0.5vw' }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ width: '5vw', marginLeft: '1vw', borderRadius: '0.5vw' }}
+            >
+              Bid
+            </Button>
+          </Grid>
+          <Grid item container="true" justify="center" style={{ marginTop: '1vw' }}>
+            <input
+              type="number"
+              value={parseInt(auction.stealPrice, 10)}
+              style={{ width: '5vw', borderRadius: '0.5vw' }}
+              readOnly
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ width: '5vw', marginLeft: '1vw', borderRadius: '0.5vw' }}
+            >
+              Steal
+            </Button>
+          </Grid>
         </Grid>
       </Card>
+      <script type="text/javascript" src="../myfunctions.js" />
     </Grid>
   );
 }
--- src/myFunctions.js
+++ src/myFunctions.js
@@ -1,17 +1,18 @@
-const formatDate = (oldDate) => {
-    let newDate = (oldDate.getMonth() + 1) < 10 ? "0" + (oldDate.getMonth() + 1) : (oldDate.getMonth() + 1);
-    newDate += '/';
-    newDate += oldDate.getDate() < 10 ? "0" + oldDate.getDate() : oldDate.getDate();
-    newDate += '/';
-    newDate += oldDate.getFullYear();
-    newDate += ' | ';
-    newDate += oldDate.getHours() < 10 ? "0" + oldDate.getHours() : oldDate.getHours();
-    newDate += ':';
-    newDate += oldDate.getMinutes() < 10 ? "0" + oldDate.getMinutes() : oldDate.getMinutes();
-    newDate += ':';
-    newDate += oldDate.getSeconds() < 10 ? "0" + oldDate.getSeconds() : oldDate.getSeconds();
+const formatDate = (refDate) => {
+  const oldDate = new Date(refDate);
+  let newDate = oldDate.getMonth() + 1 < 10 ? `0${oldDate.getMonth() + 1}` : oldDate.getMonth() + 1;
+  newDate += '/';
+  newDate += oldDate.getDate() < 10 ? `0${oldDate.getDate()}` : oldDate.getDate();
+  newDate += '/';
+  newDate += oldDate.getFullYear();
+  newDate += ' | ';
+  newDate += oldDate.getHours() < 10 ? `0${oldDate.getHours()}` : oldDate.getHours();
+  newDate += ':';
+  newDate += oldDate.getMinutes() < 10 ? `0${oldDate.getMinutes()}` : oldDate.getMinutes();
+  newDate += ':';
+  newDate += oldDate.getSeconds() < 10 ? `0${oldDate.getSeconds()}` : oldDate.getSeconds();
 
-    return newDate;
+  return newDate;
 };
 
 exports.formatDate = formatDate;
