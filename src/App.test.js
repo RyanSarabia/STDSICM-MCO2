50c03543c51c47b06ecd7d761c923d5d45bf5e45
robi <robi_jeanne_banogon@dlsu.edu.ph>
2021-01-20 15:21:54 +0800

    edited app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,18 @@
-/* eslint-disable import/extensions */
-/** import React from 'react';
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App'; */
-// eslint-disable-next-line import/no-unresolved
-import { formatDate } from './myFunctions';
+import App from './App';
+// import { formatDate } from './myFunctions';
 
-/** test('renders learn react link', () => {
+test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-}); */
-
-describe('Format Date', () => {
-  it("When the input is a new date '2020-12-11T04:07:31.000+00:00', it should return '12/11/2020 | 12:07:31'", () => {
-    const olddate = new Date('2020-12-11T04:07:31.000+00:00');
-    const newdate = formatDate(olddate);
-    expect(newdate).toEqual('12/11/2020 | 12:07:31');
-  });
 });
+
+// describe('Format Date', () => {
+//   it("When the input is a new date '2020-12-11T04:07:31.000+00:00', it should return '12/11/2020 | 12:07:31'", () => {
+//     const olddate = new Date('2020-12-11T04:07:31.000+00:00');
+//     const newdate = formatDate(olddate);
+//     expect(newdate).toEqual('12/11/2020 | 12:07:31');
+//   });
+// });
