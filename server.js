148a68eed2463496068e062836beec4619c0494d
robi <robi_jeanne_banogon@dlsu.edu.ph>
2021-01-25 18:39:26 +0800

    edited build path.

--- server.js
+++ server.js
@@ -58,14 +58,11 @@ app.use((req, res, next) => {
   next();
 });
 
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.static(buildPath));
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(buildPath));
+  app.use(express.static('/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(buildPath);
+    res.sendFile(path.join(`${__dirname}/build/index.html`));
   });
 }
 
