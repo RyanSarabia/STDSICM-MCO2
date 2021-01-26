4f54e56715921813e923d16cb48e368b50261dc4
robi <robi_jeanne_banogon@dlsu.edu.ph>
2021-01-26 18:37:16 +0800

    edited register routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,8 +3,8 @@ const indexController = require('../controller/indexController');
 
 router.get('/api/auth/google', indexController.signin);
 router.get('/api/auth/google/callback', indexController.callback, indexController.callbackSuccess);
-router.get('/api/register', indexController.getRegister);
-router.post('/api/register', indexController.postRegister);
+router.get('/api/newuser', indexController.getRegister);
+router.post('/api/newuser', indexController.postRegister);
 router.get('/api/validate', indexController.userIsLoggedIn);
 router.get('/api/getID', indexController.getID);
 router.get('/api/logout', indexController.logout);
