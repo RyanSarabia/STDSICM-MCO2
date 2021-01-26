f34a6ae2219d8ef27fff46751ad0999ab95491c4
robi <robi_jeanne_banogon@dlsu.edu.ph>
2021-01-26 16:38:33 +0800

    edited routes

--- server.js
+++ server.js
@@ -57,14 +57,6 @@ app.use((req, res, next) => {
   next();
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(`${__dirname}/build`)));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/build/index.html`));
-  });
-}
-
 app.use('/', indexRoute);
 app.use('/login', indexRoute);
 app.use('/register', UserAuth.userIsLoggedIn, UserAuth.userIsNew, indexRoute);
@@ -73,6 +65,14 @@ app.use('/upload', userRoute);
 app.use('/create', userRoute);
 app.use('/profile', userRoute);
 app.use('/auction', userRoute);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(`${__dirname}/build`)));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/build/index.html`));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
