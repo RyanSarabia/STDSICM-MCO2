f572b9022825fc3be73f6076e9b8a666c7c3323b
robi <robi_jeanne_banogon@dlsu.edu.ph>
2021-01-26 14:25:52 +0800

    edited passport

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: '566380691468-ur49vuf9v71l7juso330av435mttobgk.apps.googleusercontent.com',
       clientSecret: '0d1Dr1wYejWhyw9e2NhbOOZr',
-      callbackURL: 'http://localhost:5000/auth/google/callback',
+      callbackURL: '/api/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
       const userData = {
--- server.js
+++ server.js
@@ -59,13 +59,13 @@ app.use((req, res, next) => {
   next();
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(`${__dirname}/build`)));
+// if (process.env.NODE_ENV === 'production') {
+app.use('*', express.static(path.join(`${__dirname}/build`)));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/build/index.html`));
-  });
-}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/build/index.html`));
+});
+// }
 
 app.use('/', indexRoute);
 app.use('/login', indexRoute);
