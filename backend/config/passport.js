214698e7340d8ad30ef09837b8fe56a1213cf79f
mikobhoie2299 <ryan_miguel_sarabia@dlsu.edu.ph>
2021-09-08 06:26:06 +0800

    Update passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -14,7 +14,7 @@ passport.use(
     {
       clientID: '276325560458-bhdrtn5e72d2t1jf8puhqfdcol18dvhd.apps.googleusercontent.com',
       clientSecret: 'hym1clsI47pSwqMvNuVX4Zi1',
-      callbackURL: '/api/auth/google/callback',
+      callbackURL: 'https://localhost:5000/api/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
       const userData = {
